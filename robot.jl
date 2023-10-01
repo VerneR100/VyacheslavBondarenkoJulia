3e4b806561f3c919090d9fbdc8cd54e3871d3cf3
VerneR100 <110999162+VerneR100@users.noreply.github.com>
2023-10-02 01:06:02 +0300

    Update robot.jl

--- robot.jl
+++ robot.jl
@@ -1,4 +1,14 @@
 # Создание объекта типа Robot
 using HorizonSideRobots
 
-r = Robot("untitled.sit"; animate = true)
+function field_4()
+    return Robot("untitled4.sit", animate = true)
+end
+
+function field_5()
+    return Robot("untitled5.sit", animate = true)
+end
+
+function field_6()
+    return Robot("untitled6.sit", animate = true)
+end
